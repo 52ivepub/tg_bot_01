4a7f8fc58f7909b4ec9842ea0b46913268dec7b4
PC_evgenyi <abusyginza@gmail.com>
2025-03-14 20:31:48 +0700

    weather_bot_01

--- main_03.py
+++ main_03.py
@@ -1,4 +1,6 @@
 import telebot
+import requests
+import json
 
 bot = telebot.TeleBot("7672525355:AAE-TEINCA5rdlFbxIkXvHJvdri15lfY6zg")
 API = 'd4caf08186cd9d2e731ea09ddb14723e'
@@ -14,6 +16,18 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
     city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if res.status_code == 200:    
+        data = json.loads(res.text)
+        temp = data['main']['temp']
+        bot.reply_to(message, f'Сейчас погода: {temp }')
+        image = "sun.jpg" if temp > 5.0 else 'cloud.png'
+        file = open('./' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.reply_to(message, f'Город указан неверно')
+
+
 
 
 
