fc7f94f25fda7ea64410149a8230b18c6853fec7
PC_evgenyi <abusyginza@gmail.com>
2025-03-26 20:24:15 +0700

    echo

--- PyHS/main_05.py
+++ PyHS/main_05.py
@@ -18,21 +18,24 @@ async def start(message: types.Message):
 
 
 @dp.message()
-async def echo(message: types.Message):
+async def echo(message: types.Message, bot:Bot):
     """Эхо"""
+    await bot.send_message(message.from_user.id, 'Ответ')
     text = message.text
-    if text in ["Привет", "привет", "hi", "hello"]:
-        await message.answer('И тебе привет')
-    elif text in ["Пока", "пока", "пакеда", "До свидания"]:
-        await message.answer("И тебе пока")
-    else:
-        await message.answer(message.text)
+    # if text in ["Привет", "привет", "hi", "hello"]:
+    #     await message.answer('И тебе привет')
+    # elif text in ["Пока", "пока", "пакеда", "До свидания"]:
+    #     await message.answer("И тебе пока")
+    # else:
+    await message.answer(message.text)
+    await message.reply(message.text)
     
     
 
 
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
