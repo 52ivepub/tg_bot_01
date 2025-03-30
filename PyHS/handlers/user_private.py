e92a98c452941e47cd1754a0e7b99b8fbddca3a8
PC_evgenyi <abusyginza@gmail.com>
2025-03-30 07:48:19 +0700

    group

--- PyHS/handlers/user_private.py
+++ PyHS/handlers/user_private.py
@@ -2,8 +2,8 @@
 
 
 
-from aiogram import Bot, Router, types
-from aiogram.filters import Command
+from aiogram import F, Bot, Router, types
+from aiogram.filters import Command, or_f
 
 user_private_router = Router()
 
@@ -12,7 +12,32 @@ async def start(message: types.Message):
     await message.answer('Привет, я  вирутальный помощник')
 
 
-@user_private_router.message(Command('menu'))
-async def echo(message: types.Message, bot:Bot):
+@user_private_router.message(or_f(Command('menu'), (F.text.lower() == 'меню'))) 
+async def menu(message: types.Message):
     """Эхо"""
     await message.answer('Вот меню:')
+
+
+@user_private_router.message(F.text.lower() == 'о нас')
+@user_private_router.message(Command('about'))
+async def about(message: types.Message):
+    await message.answer('О нас:')
+
+
+@user_private_router.message(F.text.lower() == 'варианты оплаты')
+@user_private_router.message(Command('payment'))
+async def payment(message: types.Message):
+    await message.answer('Варианты оплаты:')
+
+
+@user_private_router.message(F.text.lower().contains('доставк'))
+@user_private_router.message(Command('shipping'))
+async def shipping(message: types.Message):
+    await message.answer('Варианты доставки:')
+
+
+# @user_private_router.message(F.text.lower().contains('доставк'))
+# async def shipping(message: types.Message):
+#     await message.answer('Это магический фильтр')
+
+
--- PyHS/main_05.py
+++ PyHS/main_05.py
@@ -6,6 +6,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from dotenv import find_dotenv, load_dotenv
 from handlers.user_private import user_private_router
+from common.bot_cmds_list import private
+from handlers.user_group import user_group_router
 
 
 load_dotenv(find_dotenv())
@@ -14,7 +16,9 @@ ALOWED_UPDATES = ['message, edited_message']
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
+
 dp.include_routers(user_private_router)
+dp.include_routers(user_group_router)
 
 
 
@@ -23,6 +27,8 @@ dp.include_routers(user_private_router)
  
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats()) если нужно изменить кнопки меню
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALOWED_UPDATES)
 
 
